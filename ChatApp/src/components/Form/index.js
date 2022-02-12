ede6e474688e9f3cbc1cf05975a6bbe69985b3d7
soshirokov <soshirokov@gmail.com>
2022-02-12 16:33:33 +0300

    l-5

---
+++ ChatApp/src/components/Form/index.js
@@ -0,0 +1,27 @@
+ import TextField from '@mui/material/TextField';
+ import Button from '@mui/material/Button';
+ import './style.scss';
+import { useState } from 'react';
+
+ export default function Form({onSubmitCallback, placeholder, submitText}) {
+    const [value, setValue] = useState('');
+    
+    function submitHandler (e) {
+        e.preventDefault();
+        onSubmitCallback(value);
+        setValue('');
+    }
+
+    function inputChangeHandler(e) {
+        setValue(e.target.value);
+    }
+
+     return (
+         <div className='form' onSubmit={submitHandler}>
+             <form>
+                 <TextField className='form__input' id="standard-basic" label={placeholder} value={value} variant="standard" onChange={inputChangeHandler}  autoFocus/>
+                 <Button className='form__submit' variant="contained" type='submit'>{submitText}</Button>
+             </form>
+         </div>
+     );
+ } 
---
+++ ChatApp/src/store/profile/reducer.js
@@ -0,0 +1,23 @@
+import { TOGGLE_SHOW_NAME, SET_NAME } from './actions'
+
+const initialState = {
+    showName: false,
+    name: 'Default'
+  }
+  
+export const profileReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case TOGGLE_SHOW_NAME:
+        return {
+            ...state,
+            showName: !state.showName
+        }
+        case SET_NAME:
+        return {
+            ...state,
+            name: action.name
+        }
+        default:
+            return state
+    }
+}
